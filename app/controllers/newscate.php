48ce92a15f3de707f3b19a7de8e432223d32dd8a
hieu2211 <h.vanhieu1999@gmail.com>
2020-07-20 09:23:42 +0700

    hieu

--- app/controllers/newscate.php
+++ app/controllers/newscate.php
@@ -2,6 +2,6 @@
 include_once '../app/init.php';
 class newscate extends model_and_view_newscate {
     function index() {
-        $this->view('index',"data");
+        $this->view('Index');
     }
 }
