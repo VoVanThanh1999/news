9831ad86e67602dc78a10ae6dd5c138dbb28a530
hieu2211 <h.vanhieu1999@gmail.com>
2020-07-20 09:14:46 +0700

    hieu

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -3,7 +3,7 @@ include_once '../app/init.php';
 
 class home extends model_and_view_user {
     public function  index() {
-        $this->view('trangchu',"home");
+        $this->view('trangchu',"");
     }
     public  function pages(){
         echo  'pages';
