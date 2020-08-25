c87dd56e97dc86c1a2525db72a7466b9fa5d3d76
thutran201198 <thutran201198>
2020-08-25 22:36:47 +0700

    update chi tiet

---
+++ public/views/users/conment_details.php
@@ -0,0 +1,17 @@
+<div class="comment-list">
+	<div class="single-comment justify-content-between d-flex">
+		<div class="user justify-content-between d-flex">
+			<div class="thumb">
+				<img src="img/blog/c1.jpg" alt="">
+			</div>
+			<div class="desc">
+				<h5>
+					<a href="#">Emilly Blunt</a>
+				</h5>
+				<p class="date">December 4, 2017 at 3:12 pm</p>
+				<p class="comment">Never say goodbye till the end comes!</p>
+			</div>
+		</div>
+		
+	</div>
+</div>
